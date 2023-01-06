012e902188ab658473e2fd1f5be586337196c5d2
unknown <punitagarwalakalna@gmail.com>
2023-01-06 14:22:11 +0530

    first version of goodday code

---
+++ goodday.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(void){
+    string s;
+    cout<<"is it ur good day?"<<endl;
+    cin>>s;
+    if(s=="yes")
+        cout<<"well, carry one"<<endl;
+    else
+        cout<<"let's make it a good day"<<endl;
+    return 0;
+}
